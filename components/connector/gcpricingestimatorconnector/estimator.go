775db9e8f238b88629e7b47bb1b0f18fdd5eb79f
Robbie Lankford <robert.lankford@grafana.com>
2023-03-15 18:23:22 -0700

    go fmt

--- components/connector/gcpricingestimatorconnector/estimator.go
+++ components/connector/gcpricingestimatorconnector/estimator.go
@@ -16,7 +16,6 @@ import (
 	"go.uber.org/zap"
 )
 
-
 type sum struct {
 	count      uint64
 	attributes pcommon.Map
@@ -47,14 +46,13 @@ func (m *sumMetrics) getOrCreate(k string, attributes pcommon.Map) *sum {
 	return s
 }
 
-
 type estimator struct {
-  lock sync.Mutex
+	lock   sync.Mutex
 	logger *zap.Logger
 
-  started bool
-	done   chan struct{}
-  ticker *time.Ticker
+	started bool
+	done    chan struct{}
+	ticker  *time.Ticker
 
 	metrics sumMetrics
 	mc      consumer.Metrics
@@ -75,128 +73,127 @@ func newEstimator(logger *zap.Logger, _ *Config) (*estimator, error) {
 }
 
 func (e *estimator) exportMetrics(ctx context.Context) {
-  e.lock.Lock()
-  defer e.lock.Unlock()
-  var pMetrics pmetric.Metrics
+	e.lock.Lock()
+	defer e.lock.Unlock()
+	var pMetrics pmetric.Metrics
 
-  // tada?
+	// tada?
 
-  if err := e.mc.ConsumeMetrics(ctx, pMetrics); err != nil {
-    e.logger.Error("Failed ConsumeMetrics", zap.Error(err))
-  }
+	if err := e.mc.ConsumeMetrics(ctx, pMetrics); err != nil {
+		e.logger.Error("Failed ConsumeMetrics", zap.Error(err))
+	}
 }
 
 func (e *estimator) Start(ctx context.Context, host component.Host) error {
-  e.logger.Info("Started gcpricingestimator connector")
-  e.started = true
+	e.logger.Info("Started gcpricingestimator connector")
+	e.started = true
 
-  go func() {
-    for {
-      select {
-      case <-e.done:
-        return
-      case <-ctx.Done():
-        return
-      case <-e.ticker.C:
-        e.exportMetrics(ctx)
-      }
-    }
-  }()
+	go func() {
+		for {
+			select {
+			case <-e.done:
+				return
+			case <-ctx.Done():
+				return
+			case <-e.ticker.C:
+				e.exportMetrics(ctx)
+			}
+		}
+	}()
 
-  return nil
+	return nil
 }
 
 func (e *estimator) Shutdown(ctx context.Context) error {
-  e.logger.Info("Stopping gcpricingestimator connector")
-  if e.started {
-    e.ticker.Stop() 
-    e.done <- struct{}{}
-    e.started = false
-  }
-  return nil
+	e.logger.Info("Stopping gcpricingestimator connector")
+	if e.started {
+		e.ticker.Stop()
+		e.done <- struct{}{}
+		e.started = false
+	}
+	return nil
 }
-
 
 func (*estimator) Capabilities() consumer.Capabilities {
 	return consumer.Capabilities{MutatesData: false}
 }
 
 func (e *estimator) ConsumeMetrics(ctx context.Context, metrics pmetric.Metrics) error {
-  e.lock.Lock()
-  defer e.lock.Unlock()
+	e.lock.Lock()
+	defer e.lock.Unlock()
 
 	// count active series
-  m := e.metrics.getOrCreate("estimator_active_series", pcommon.NewMap())
-  m.add(uint64(metrics.MetricCount()))
+	m := e.metrics.getOrCreate("estimator_active_series", pcommon.NewMap())
+	m.add(uint64(metrics.MetricCount()))
 
-  return nil
+	return nil
 }
 
 func (e *estimator) ConsumeLogs(ctx context.Context, logs plog.Logs) error {
-  e.lock.Lock()
-  defer e.lock.Unlock()
-  m := e.metrics.getOrCreate("estimator_log_bytes", pcommon.NewMap())
+	e.lock.Lock()
+	defer e.lock.Unlock()
+	m := e.metrics.getOrCreate("estimator_log_bytes", pcommon.NewMap())
 
-  // get byte count of logs (probably VERY inaccurately for now)
-  // This iterates over the logs and adds the sum of bytes making up
-  // the body (only string types for now).
-  for i := 0; i < logs.ResourceLogs().Len(); i++ {
-    rl := logs.ResourceLogs().At(i)
-    for j := 0; j < rl.ScopeLogs().Len(); j++ {
-      sl := rl.ScopeLogs().At(j)
-      for k := 0; k < sl.LogRecords().Len(); k++ {
-        lr := sl.LogRecords().At(k)
-        switch t := lr.Body().Type(); t {
-        case pcommon.ValueTypeStr:
-          m.add(uint64(len(lr.Body().Str())))
-        }
-      }
-    }
-  }
-  m.add(uint64(unsafe.Sizeof(logs)))
-  
-  return nil
+	// get byte count of logs (probably VERY inaccurately for now)
+	// This iterates over the logs and adds the sum of bytes making up
+	// the body (only string types for now).
+	for i := 0; i < logs.ResourceLogs().Len(); i++ {
+		rl := logs.ResourceLogs().At(i)
+		for j := 0; j < rl.ScopeLogs().Len(); j++ {
+			sl := rl.ScopeLogs().At(j)
+			for k := 0; k < sl.LogRecords().Len(); k++ {
+				lr := sl.LogRecords().At(k)
+				switch t := lr.Body().Type(); t {
+				case pcommon.ValueTypeStr:
+					m.add(uint64(len(lr.Body().Str())))
+				}
+			}
+		}
+	}
+	m.add(uint64(unsafe.Sizeof(logs)))
+
+	return nil
 }
 
 func (e *estimator) ConsumeTraces(ctx context.Context, traces ptrace.Traces) error {
-  e.lock.Lock()
-  defer e.lock.Unlock()
-  m := e.metrics.getOrCreate("estimator_trace_bytes", pcommon.NewMap())
+	e.lock.Lock()
+	defer e.lock.Unlock()
+	m := e.metrics.getOrCreate("estimator_trace_bytes", pcommon.NewMap())
 
-  // get byte count of traces (probably VERY inaccurately for now)
-  // This iterates over the spans and adds the sum of bytes that make
-  // up the span attributes and span events (only string types for now).
-  for i := 0; i < traces.ResourceSpans().Len(); i++ {
-    rs := traces.ResourceSpans().At(i)
-    for j := 0; j < rs.ScopeSpans().Len(); j++ {
-      ss := rs.ScopeSpans().At(j)
-      for k := 0; k < ss.Spans().Len(); k++ {
-        span := ss.Spans().At(k)
-        sa := span.Attributes()
-        for key, val := range sa.AsRaw() {
-          m.add(uint64(len(key)))
-          vt := val.(pcommon.Value).Type()
-          switch vt {
-          case pcommon.ValueTypeStr:
-            m.add(uint64(len(val.(pcommon.Value).Str())))
-          }
-        }
+	// get byte count of traces (probably VERY inaccurately for now)
+	// This iterates over the spans and adds the sum of bytes that make
+	// up the span attributes and span events (only string types for now).
+	for i := 0; i < traces.ResourceSpans().Len(); i++ {
+		rs := traces.ResourceSpans().At(i)
+		for j := 0; j < rs.ScopeSpans().Len(); j++ {
+			ss := rs.ScopeSpans().At(j)
+			for k := 0; k < ss.Spans().Len(); k++ {
+				span := ss.Spans().At(k)
+				sa := span.Attributes()
+				for key, val := range sa.AsRaw() {
+					m.add(uint64(len(key)))
+					vt := val.(pcommon.Value).Type()
+					switch vt {
+					case pcommon.ValueTypeStr:
+						m.add(uint64(len(val.(pcommon.Value).Str())))
+					}
+				}
 
-        for h := 0; h < span.Events().Len(); h++ {
-          event := span.Events().At(h)
-          m.add(uint64(len(event.Name())))
-          for key, val := range event.Attributes().AsRaw() {
-            m.add(uint64(len(key)))
-            vt := val.(pcommon.Value).Type()
-            switch vt {
-            case pcommon.ValueTypeStr:
-              m.add(uint64(len(val.(pcommon.Value).Str())))
-            }
-          }
-        }
-      }
-    }
-  }
+				for h := 0; h < span.Events().Len(); h++ {
+					event := span.Events().At(h)
+					m.add(uint64(len(event.Name())))
+					for key, val := range event.Attributes().AsRaw() {
+						m.add(uint64(len(key)))
+						vt := val.(pcommon.Value).Type()
+						switch vt {
+						case pcommon.ValueTypeStr:
+							m.add(uint64(len(val.(pcommon.Value).Str())))
+						}
+					}
+				}
+			}
+		}
+	}
 
-  return nil
+	return nil
 }
